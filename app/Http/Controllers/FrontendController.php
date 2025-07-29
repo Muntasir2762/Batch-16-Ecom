95924f0e32a88012925d43ff1a3187729ee47df5
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-07-30 00:09:55 +0600

    frontend is in progress

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Order;
 use App\Models\OrderDetails;
 use App\Models\Product;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -22,9 +23,27 @@ class FrontendController extends Controller
         return view('frontend.index', compact('hotProducts', 'newProducts', 'regularProducts', 'discountProducts', 'categories'));
     }
 
+    public function categoryProducts ($slug, $id)
+    {
+        $category = Category::find($id);
+        $products = Product::where('cat_id',$id)->get();
+        $productsCount = Product::where('cat_id',$id)->count();
+        return view('frontend.category-products', compact('products', 'productsCount', 'category'));
+    }
+
+    public function subCategoryProducts ($slug, $id)
+    {
+        $subCategory = SubCategory::find($id);
+        $products = Product::where('sub_cat_id',$id)->get();
+        $productsCount = Product::where('sub_cat_id',$id)->count();
+        return view('frontend.subcategory-products', compact('subCategory', 'products', 'productsCount'));
+    }
+
     public function shopProducts ()
     {
-        return view('frontend.shop');
+        $products = Product::orderBy('id', 'desc')->get();
+        $productsCount = Product::orderBy('id', 'desc')->count();
+        return view('frontend.shop', compact('productsCount', 'products'));
     }
 
     public function returnProcess ()
